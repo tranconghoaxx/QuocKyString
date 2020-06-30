d26b79394bc99152421d073e44ffc0bbe992ed19
tranconghoaxx <tranhoa300696@gmail.com>
2020-06-30 22:51:06 +0700

    demo arrayadapter

--- java/com/example/quockystring/MainActivity.java
+++ java/com/example/quockystring/MainActivity.java
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvdanhSach;
-    Spinner dsSpiner;
-    ArrayList<String>data = new ArrayList<>();
-//    ArrayAdapter adapter;
+    ArrayList<QuocKy>data = new ArrayList<>();
+//    CustomAdapter adapter;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +24,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void setEvent() {
         KhoiTao();
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,data);
+        adapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,data);
+//        adapter = new CustomAdapter(this,R.layout.listview_item,data);
         lvdanhSach.setAdapter(adapter);
     }
 
     private void KhoiTao() {
-        data.add("CongHoa");
-        data.add("NguyenA");
-        data.add("TranB");
+        QuocKy qk1 = new QuocKy();
+        qk1.setCo(R.drawable.vietnam);
+        qk1.setTen("Việt Nam");
+        data.add(qk1);
+
+        QuocKy qk2 = new QuocKy();
+        qk2.setCo(R.drawable.england);
+        qk2.setTen("England");
+        data.add(qk2);
+
+        QuocKy qk3 = new QuocKy();
+        qk3.setCo(R.drawable.us);
+        qk3.setTen("US");
+        data.add(qk3);
     }
 
     private void setControl() {
         lvdanhSach = findViewById(R.id.listView);
-        dsSpiner = findViewById(R.id.spiner);
     }
 }
